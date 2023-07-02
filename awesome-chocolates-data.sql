38908257d9e5d09ff815ea7dd3a0d746b173c6bb
15josh08 <joshrajendrapatel@gmail.com>
2023-07-02 11:55:32 -0400

    loaded the data to local host

--- awesome-chocolates-data.sql
+++ awesome-chocolates-data.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE  IF NOT EXISTS `awesome chocolates` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `awesome chocolates`;
+-- CREATE DATABASE  IF NOT EXISTS `awesome chocolates` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE chocolate;
 -- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
 --
 -- Host: localhost    Database: awesome chocolates
